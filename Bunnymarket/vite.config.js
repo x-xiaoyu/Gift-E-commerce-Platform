de528557e418c340cb3e2bbe9a5012be3af76135
xiaoxy <xiaoxiaoyu0226@gmail.com>
2026-04-23 21:21:56 -0400

    element自动按需导入+主题色定制

--- Bunnymarket/vite.config.js
+++ Bunnymarket/vite.config.js
@@ -3,16 +3,39 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+// elementPulus按需导入
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [
+        // 1. 配置elementPlus采用sass样式配色系统
+        ElementPlusResolver({importStyle:"sass"})],
+    }),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  css: {
+    preprocessorOptions: {
+      // 2. 自动导入定制化样式文件进行样式覆盖
+      scss: {
+        additionalData: `
+          @use "@/styles/element/index.scss" as *;
+        `,
+      },
+    },
+  }
+
 })
